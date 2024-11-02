02245ee8b116f0671e36b447eb923e545c146738
Duke <naim.giger@outlook.com>
2024-11-02 13:36:12 +0100

    upgrade kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
+    application
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 group = "ch.dukex"
@@ -27,5 +28,5 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
